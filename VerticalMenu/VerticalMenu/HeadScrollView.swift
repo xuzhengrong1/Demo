15d610c473f1f248b03998f436761b057b258d65
许正荣 <842841022@qq.com>
2017-05-26 18:26:25 +0800

    按钮显示

---
+++ VerticalMenu/VerticalMenu/HeadScrollView.swift
@@ -0,0 +1,56 @@
+//
+//  HeadScrollView.swift
+//  VerticalMenu
+//
+//  Created by 许正荣 on 2017/5/26.
+//  Copyright © 2017年 许正荣. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class HeadScrollView: UIView {
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+
+    var headScrollView: UIScrollView?
+
+    override func willMove(toSuperview newSuperview: UIView?) {
+        self.headScrollView?.addObserver(self, forKeyPath: "contentOffset", options: .new, context: nil)
+        self.headScrollView?.contentInset = UIEdgeInsetsMake(231, 0, 0, 0)
+
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        let point = change?[.newKey]
+        self.updateSubViewWithScrollOffset(point as! CGPoint);
+
+
+    }
+
+    
+    
+    func updateSubViewWithScrollOffset(_ offset: CGPoint) {
+        var newOffset = offset
+         let bottomHeight =  -self.headScrollView!.contentInset.top
+        
+        let  y = (newOffset.y < bottomHeight) ? bottomHeight : (newOffset.y > -105 ? -105 : newOffset.y) //间距小于64选64， 高度大于两百四选两百四，之前选期间选高度值
+        newOffset = CGPoint(x: newOffset.x, y: y)
+        
+        let newY = -newOffset.y - 231;
+        let topBottomMargin = -105  - bottomHeight ;
+        let alpha  =  1 - (newOffset.y - bottomHeight) / topBottomMargin
+        
+        self.frame = CGRect(x: 0, y: newY, width: self.frame.size.width, height: self.frame.size.height)
+        
+        topView.frame = CGRect(x: 0, y: -newY, width: topView.frame.size.width, height: topView.frame.size.height)
+    
+        
+        print("alpha== %f",alpha)
+        topView.alpha = 1 - alpha;
+        bottomView.alpha = alpha;
+        
+    
+    }
+
+
+}
--- VerticalMenu/VerticalMenu/ViewController.swift
+++ VerticalMenu/VerticalMenu/ViewController.swift
@@ -8,12 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
        var  button = TestButton()
       let memu =  XZRHorizontalMenu()
     
+    var tableView: UITableView?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+      tableView = UITableView(frame: self.view.bounds, style: .plain)
+        tableView?.delegate = self
+        tableView?.dataSource = self;
+                tableView?.register( UITableViewCell.self, forCellReuseIdentifier: "cell")
+       let View =  UINib(nibName: "HeadScrollView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! HeadScrollView
+        View.frame = CGRect(x: 0, y: 0, width: self.view.size.width, height: 231);
+         View.headScrollView = tableView;
+        self.view.addSubview(View);
+        self.view.addSubview(tableView!)
+    
+        self.view.bringSubview(toFront: View)
+
+        
+        
+    
+
+    }
+    
+    
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1 ;
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20 ;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath);
+       cell.textLabel?.text = "111";
+        return cell
+        
+    }
+    
+    
+    func tapped() {
+       
+        
         button.frame = CGRect(x: 200, y: 100, width: 40, height: 120)
         button.backgroundColor = UIColor.red;
         button.addTarget(self, action: #selector(tapped), for: .touchUpInside)
@@ -25,9 +68,9 @@ class ViewController: UIViewController {
         }
         
         memu.addButton("推荐11", target: self, selector: #selector(test))
-//        memu.addButton("11", backgroundColor: UIColor.red, textColor: UIColor.white, showDurationStatus: false) {
-//            
-//        }
+        //        memu.addButton("11", backgroundColor: UIColor.red, textColor: UIColor.white, showDurationStatus: false) {
+        //
+        //        }
         let btn = memu.addButton("11", backgroundColor: UIColor.red, textColor: UIColor.white, showDurationStatus: false) {
             
         }
@@ -40,13 +83,9 @@ class ViewController: UIViewController {
         }
         btn.drawSplitLine = false
         
-       let pick =  CustomDatePickerView(frame: CGRect(x: 0, y: 100, width: self.view.size.width, height:300))
+        let pick =  CustomDatePickerView(frame: CGRect(x: 0, y: 100, width: self.view.size.width, height:300))
         
         self.view.addSubview(pick)
-
-    }
-    
-    func tapped() {
                 memu.showMenuInView(button);
     }
 
